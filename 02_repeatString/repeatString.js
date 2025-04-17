de5aa62f6c7660b8704dd9c1a284d7123fadcdf3
elifnull <Elifnull@protonmail.com>
2025-04-17 14:36:52 -0400

    code updated to comply with tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+   let sentance ="";
+   if (num >= 0){
+        while (num > 0){
+            sentance += string;
+            num--;
+        }
+        return sentance;
+    } else if (num < 0){
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
