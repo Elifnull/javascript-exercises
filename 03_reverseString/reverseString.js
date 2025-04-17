d409dd5ec39a34a7a97eea0ceedd1fa95d07fbca
elifnull <Elifnull@protonmail.com>
2025-04-17 15:03:57 -0400

    Code updated to comply with tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let outputString = "";
+    while( stringLength > 0){
+        outputString+=string.charAt(stringLength-1);
+        stringLength--;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
