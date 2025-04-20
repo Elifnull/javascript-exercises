550c7ccc1ee02e36e6f113ae89ffd66f194732a7
elifnull <Elifnull@protonmail.com>
2025-04-19 22:46:14 -0400

    consoldated Filter method with includes method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeNum) {
+    let removeArray = [...removeNum];
+    let finalArray = array.filter((number)=>!removeArray.includes(number));
+    console.log(finalArray);
+    return finalArray;
 };
+
+
+removeFromArray([1,2,4,5,6,7,4],1,2,3,4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
