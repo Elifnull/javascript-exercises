4a096127c45bb4ee7b3c09469a6b2d9023c328de
elifnull <Elifnull@protonmail.com>
2025-04-20 00:54:42 -0400

    added conversion and rouding methods

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius =(temp - 32)*(5/9)
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+ let fahrenheit = (temp *9/5) + 32;
+ return Number(fahrenheit.toFixed(1));
 };
 
+
+convertToCelsius(33);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
