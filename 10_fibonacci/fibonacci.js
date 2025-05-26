28430f015eb7ecf2986db7e0ec3193d7b270058e
elifnull <Elifnull@protonmail.com>
2025-05-25 23:42:30 -0400

    updated Fib test; implemented reduce and map for section 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num1) {
+    const num = parseInt(num1);
+    if( typeof num === "number" && num > -1){
+        const fibonacciArray = [0,1,1];
+        if (num < 3){
+            return fibonacciArray[num];
+        }
+        else {
+        for(let i=3; i <= num; i++){
+            fibonacciArray[i] = fibonacciArray[i-1]+fibonacciArray[i-2];
+        }
+        }
+        return fibonacciArray[num];
+     } else {
+        return "OOPS";
+     }
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles01 = function(array) {
+    return array.reduce((cummilative, current) =>{
+        cummilative.push(current["title"]);
+        return cummilative;
+    },[]);
+};
 
+const getTheTitles = function(array){
+    return array.map((array) => array["title"]);
 };
 
 // Do not edit below this line
