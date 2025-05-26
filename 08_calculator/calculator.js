6358720370c45b3997bc1664477e8f032c8450f8
elifnull <Elifnull@protonmail.com>
2025-05-25 22:11:11 -0400

    solved calculator with 2 pointer algo

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1+num2;
+};
+
+const subtract = function(num1, num2) {
+  return num1-num2;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+ /*
+  let summation = 0;
+  for (let element of array){
+    summation += element;
+  }
+  return summation;
+	*/
+  return array.reduce((cumilitive, current)=> cumilitive+=current,0);
+};
+
+const multiply = function(array) {
+  return array.reduce((cumilitive,current)=> cumilitive*=current)
+};
+
+const power = function(num1, num2) {
+  return num1**num2;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num1) {
+  if (num1 == 0) {
+    return 1;
+  } else {
+    return num1 * factorial(num1-1);
+  }
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
 
+
+const palindromes = function (string) {
+    const lettersOnlyString = string.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    const stringLength = lettersOnlyString.length;
+    let left = 0;
+    let right = stringLength - 1;
+
+    while (left < right){
+        if(lettersOnlyString[left]===lettersOnlyString[right]){
+            left++;
+            right--;
+        } else {
+            return false;
+        }
+    } return true;
 };
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
