bc037b5d054829acf5bd39170753da8b6bad48f2
elifnull <Elifnull@protonmail.com>
2025-05-30 13:08:37 -0400

    reduce output changed to object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,15 @@
-const findTheOldest = function(array) {
-    
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = (oldestPerson.yearOfDeath || currentYear) - oldestPerson.yearOfBirth;
+        const currentAge = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 12_findTheOldest/solution/findTheOldest-solution.js
+++ 12_findTheOldest/solution/findTheOldest-solution.js
@@ -8,14 +8,30 @@ const getAge = function (birth, death) {
 const findTheOldest = function (people) {
   return people.reduce((oldest, currentPerson) => {
     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
-    const currentAge = getAge(
-      currentPerson.yearOfBirth,
-      currentPerson.yearOfDeath
-    );
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
     return oldestAge < currentAge ? currentPerson : oldest;
   });
 };
 
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+console.log(findTheOldest(people));
 /* ALTERNATIVE SOLUTION
 const getAge = function (person) {
   // The nullish coalescing assignment operator
